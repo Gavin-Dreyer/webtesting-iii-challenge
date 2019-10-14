ab32f908ad3466048192569bdebf86552b7e02ff
Gavin Dreyer <gavindreyer1@gmail.com>
2019-10-14 13:17:49 -0700

    Initial commit with tests testing display after click events were fired

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,27 @@
-// Test away!
+import React from 'react'
+import { render, fireEvent, queryAllByText } from '@testing-library/react';
+
+import Controls from './Controls'
+
+test('it renders correctly', () => {
+    // do snapshots here if you're not in codesandbox
+    render(<Controls />);
+});
+
+// test('', ()=> {
+    // const mockOpenCloseButton = jest.fn()
+    // const { getByText } = render(<Controls />);
+    // getByText(/close gate/i)
+    // fireEvent.click(getByText(/close gate/i))
+    // expect(getByText(/close gate/i)).toBe(getByText(/lock gate/i))
+
+
+
+    // const { getByText } = render(
+    //     <Controls />
+    // )
+
+    // fireEvent.click(getByText(/close gate/i))
+    // expect(/close gate/i).toHaveBeenCalled()
+// })
+
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,64 @@
-// Test away
+import React from 'react'
+import { render, fireEvent, queryAllByText } from '@testing-library/react';
+
+import Dashboard from './Dashboard'
+import { get } from 'http';
+
+test('it renders correctly', () => {
+    render(<Dashboard />);
+});
+
+test('displays current display', () => {
+    const { getByText } = render(<Dashboard />)
+
+    getByText(/open/i)
+    getByText(/unlocked/i)
+    getByText(/close gate/i)
+    getByText(/lock gate/i)
+})
+
+test('displays current display after clicking close', () => {
+    const { getByText } = render(<Dashboard />)
+
+    fireEvent.click(getByText(/close gate/i))
+    getByText(/closed/i)
+    getByText(/unlocked/i)
+    getByText(/open gate/i)
+    getByText(/lock gate/i)
+})
+
+test('displays current display after clicking lock gate', () => {
+    const { getByText } = render(<Dashboard />)
+
+    fireEvent.click(getByText(/close gate/i))
+    fireEvent.click(getByText(/lock gate/i))
+    getByText(/closed/i)
+    getByText(/locked/i)
+    getByText(/open gate/i)
+    getByText(/unlock gate/i)
+})
+
+test('displays current display after clicking lock gate', () => {
+    const { getByText } = render(<Dashboard />)
+
+    fireEvent.click(getByText(/close gate/i))
+    fireEvent.click(getByText(/lock gate/i))
+    fireEvent.click(getByText(/unlock gate/i))
+    getByText(/closed/i)
+    getByText(/unlocked/i)
+    getByText(/open gate/i)
+    getByText(/lock gate/i)
+})
+
+test('displays current display after clicking lock gate', () => {
+    const { getByText } = render(<Dashboard />)
+
+    fireEvent.click(getByText(/close gate/i))
+    fireEvent.click(getByText(/lock gate/i))
+    fireEvent.click(getByText(/unlock gate/i))
+    fireEvent.click(getByText(/open gate/i))
+    getByText(/open/i)
+    getByText(/unlocked/i)
+    getByText(/close gate/i)
+    getByText(/lock gate/i)
+})
